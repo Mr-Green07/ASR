ff540ed3b35733d895dfbf546030dad97db4e5de
Mr-Green07 <yuvrajkumar5070@gmail.com>
2026-04-10 00:08:49 +0530

    initial commit

---
+++ audio.py
@@ -0,0 +1,21 @@
+from RealtimeSTT import AudioToTextRecorder
+
+if __name__ == '__main__':
+    print("Initializing Model (this takes 10 seconds)...")
+
+    # We configure it for your 4GB GPU (cuda + int8)
+    # "large-v3-turbo" is the best balance of speed/quality for live
+    recorder = AudioToTextRecorder(
+        model="large-v3-turbo",
+        language="en",
+        device="cuda", 
+        compute_type="int8",
+        spinner=False  # Turn off the loading spinner for cleaner text
+    )
+
+    print("🔴 Ready! Speak into your microphone...")
+    print("Press Ctrl+C to stop.")
+
+    while True:
+        # This will block until you finish a sentence, then print the text
+        print(recorder.text())
---
+++ first.py
@@ -0,0 +1,6 @@
+nums = [1, 2,5,5]
+for i in range(len(nums)):
+    if i <= 2:
+        print(nums[i])
+    else:
+        print()
