14583cb8c9305b979a7e0edbca538ca6f53ab5f6
Mr-Green07 <yuvrajkumar5070@gmail.com>
2026-04-30 23:09:15 +0530

    code updated

---
+++ src/voice_assistant/tts/exceptions.py
@@ -0,0 +1,23 @@
+class TTSException(Exception):
+    """Base exception for TTS module"""
+    pass
+
+
+class AudioPlaybackError(TTSException):
+    """Raised when audio playback fails"""
+    pass
+
+
+class SynthesisError(TTSException):
+    """Raised when text-to-speech synthesis fails"""
+    pass
+
+
+class VoiceNotFoundError(TTSException):
+    """Raised when requested voice is not available"""
+    pass
+
+
+class InvalidAudioFormatError(TTSException):
+    """Raised when audio format is not supported"""
+    pass
